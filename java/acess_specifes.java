74ae41724c809f3f6b2f13afc62ff07eb052ee20
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-06-06 17:53:31 +0530

    new code added

---
+++ java/acess_specifes.java
@@ -0,0 +1,9 @@
+import package1.*;
+
+public class acess_specifes {
+    public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.publi);
+
+    }
+}
---
+++ java/package1/A.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class A {
+    public String publi = "public";
+    private String protect = "protected";
+
+    public String getProtect() {
+        return protect;
+
+    }
+
+    public void setProtect(String protect) {
+        this.protect = protect;
+
+    }
+}
